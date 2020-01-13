5b922a55091715d8aac16d53018645b8517b7889
codyphillips5 <codyphill5@gmail.com>
2020-01-13 10:38:22 -0500

    adding script functionality

---
+++ scripts/GenerateEmail.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using SeleniumProject.Utilities;
+using OpenQA.Selenium;
+
+namespace SeleniumProject.Function
+{
+	public class Script : ScriptingInterface.IScript
+	{
+		public void Execute(DriverManager driver, TestStep step)
+		{
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            int secondsSinceEpoch = (int)t.TotalSeconds;
+            long order = step.Order;
+            var emailPrefix = secondsSinceEpoch;
+            string email = emailPrefix + "@WRIGHT.EDU";
+            string wait = step.Wait != null ? step.Wait : "";
+            DataManager.CaptureMap.Add("EmailAddress", email);
+
+            List<TestStep> steps = new List<TestStep>();
+            steps.Add(new TestStep(order, "Input Generated Email", DataManager.CaptureMap["EmailAddress"], "input_text", step.By, "//input[@type='email' and @placeholder='E-mail*']", wait));
+            TestRunner.RunTestSteps(driver, null, steps);
+		}
+	}
+}
