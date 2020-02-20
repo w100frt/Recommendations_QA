f712adb4434394ba9fc777797f38a5b77c5a727e
codyphillips5 <codyphill5@gmail.com>
2020-02-20 12:38:50 -0500

    keystroke syntax

--- scripts/Backspace.cs
+++ scripts/Backspace.cs
@@ -13,7 +13,7 @@ namespace SeleniumProject.Function
 		public void Execute(DriverManager driver, TestStep step)
 		{
 			IWebElement element = driver.FindElement("xpath", "//input[@placeholder='Search a provider']");
-			element.SendKeys("^(a)");
+			element.SendKeys(Keys.Control + "a");
 			log.Info("Selecting all text");
 			element.SendKeys(Keys.Delete);
 			log.Info("Removing all text");
