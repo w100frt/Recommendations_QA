05123e29475383ffbe43271cdf0488070db5e7c3
codyphillips5 <codyphill5@gmail.com>
2020-03-02 15:06:36 -0500

    adding steps for carousel testing

---
+++ scripts/Stories.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using SeleniumProject.Utilities;
+using SeleniumProject;
+using OpenQA.Selenium;
+using log4net;
+
+namespace SeleniumProject.Function
+{
+	public class Script : ScriptingInterface.IScript
+	{
+		private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		
+		public void Execute(DriverManager driver, TestStep step)
+		{
+			long order = step.Order;
+			string wait = step.Wait != null ? step.Wait : "";
+			List<TestStep> steps = new List<TestStep>();
+			var element = driver.FindElements("xpath", "//div[contains(@class,'carousel-container card-carousel') and contains(@class,'can-scroll-right')]"); 
+			
+			if (step.Name.Equals("Click Arrow Forward to End of Carousel")) {
+				while (element.Count > 0) {
+					steps.Add(new TestStep(order, "Click Arrow Forward", "", "click", "xpath", "//button[@class='carousel-button-next image-button']", wait));
+					TestRunner.RunTestSteps(driver, null, steps);
+					steps.Clear();	
+					var element = driver.FindElements("xpath", "//div[contains(@class,'carousel-container card-carousel') and contains(@class,'can-scroll-right')]"); 				
+				}
+			}
+			
+			else {
+				log.Warn("Test Step not found in script...");
+			}
+			
+		}
+	}
+}
