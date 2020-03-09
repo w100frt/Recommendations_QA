0fbef8c4e29332ede0ccf92dbadc7fd4b8c4678e
codyphillips5 <codyphill5@gmail.com>
2020-03-09 17:27:50 -0400

    toast message

--- scripts/Favorites.cs
+++ scripts/Favorites.cs
@@ -50,7 +50,7 @@ namespace SeleniumProject.Function
 				// Allows for favoriting Leagues
 				if(step.Name.Contains("League")) {
 					steps.Add(new TestStep(order, "Favorite League", "", "click", "xpath", "//a[contains(@class,'explore-league-header')]", wait));
-					steps.Add(new TestStep(order, "Verify Toast", DataManager.CaptureMap["LEAGUE"] + " is added to your favorites.", "verify_value", "xpath", "//a[contains(@class,'explore-league-header')]", wait));
+					steps.Add(new TestStep(order, "Verify Toast", DataManager.CaptureMap["LEAGUE"] + " is added to your favorites.", "verify_value", "xpath", "//span[contains(@class,'toast-msg')]", wait));
 					TestRunner.RunTestSteps(driver, null, steps);
 					steps.Clear();
 				}
