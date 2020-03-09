bf88f4a1515fd1f221d8caa3ab1d3e47809b7d07
codyphillips5 <codyphill5@gmail.com>
2020-03-09 17:15:14 -0400

    not ncaa

--- scripts/Favorites.cs
+++ scripts/Favorites.cs
@@ -41,7 +41,7 @@ namespace SeleniumProject.Function
 				else {
 					sports = driver.FindElements("xpath", "//a[contains(@class,'entity-list-row-container')][div[div[div[not(contains(.,'NCAA'))]]]]").Count; 
 					sports = random.Next(1, sports+1);
-					steps.Add(new TestStep(order, "Click Sports Menu Open", "", "click", "xpath", "(//a[contains(@class,'entity-list-row-container')][div[div[div[(contains(.,'NCAA'))]]]])["+ sports +"]", wait));
+					steps.Add(new TestStep(order, "Click Sports Menu Open", "", "click", "xpath", "(//a[contains(@class,'entity-list-row-container')][div[div[div[not(contains(.,'NCAA'))]]]])["+ sports +"]", wait));
 					TestRunner.RunTestSteps(driver, null, steps);
 					steps.Clear();
 				}
