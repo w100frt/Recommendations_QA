ad38d22b79a1eb31e11a970ec389359a02fd3cc1
codyphillips5 <codyphill5@gmail.com>
2020-03-12 23:54:55 -0400

    non player fav

--- scripts/Favorites.cs
+++ scripts/Favorites.cs
@@ -43,12 +43,16 @@ namespace SeleniumProject.Function
 					sports = driver.FindElements("xpath", "//a[contains(@class,'entity-list-row-container')][div[div[div[(contains(.,'NFL') or contains(.,'MLB') or contains(.,'NBA') or contains(.,'NHL'))]]]]").Count; 
 					sports = random.Next(1, sports+1);
 					steps.Add(new TestStep(order, "Clicking Randomized Pro Sport", "", "click", "xpath", "(//a[contains(@class,'entity-list-row-container')][div[div[div[(contains(.,'NFL') or contains(.,'MLB') or contains(.,'NBA') or contains(.,'NHL'))]]]])["+ sports +"]", wait));
-					if (!step.Name.Contains("Player")) {
-						steps.Add(new TestStep(order, "Capture League Entity", "LEAGUE", "capture", "xpath", "//a[contains(@class,'explore-league-header')]", wait));
-					}
 					TestRunner.RunTestSteps(driver, null, steps);
 					steps.Clear();
 				}
+				
+				// Capture League for Teams
+				if (!step.Name.Contains("Player")) {
+						steps.Add(new TestStep(order, "Capture League Entity", "LEAGUE", "capture", "xpath", "//a[contains(@class,'explore-league-header')]", wait));
+						TestRunner.RunTestSteps(driver, null, steps);
+						steps.Clear();
+					}
 				
 				// Allows for favoriting Leagues
 				if(step.Name.Contains("League") || step.Name.Contains("Conference")) {
