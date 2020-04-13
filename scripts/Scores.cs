2db1cfd4788fe9948200966f169c140b1a51851a
codyphillips5 <codyphill5@gmail.com>
2020-04-13 00:34:09 -0400

    script changes

--- scripts/Scores.cs
+++ scripts/Scores.cs
@@ -16,6 +16,10 @@ namespace SeleniumProject.Function
 			long order = step.Order;
 			string wait = step.Wait != null ? step.Wait : "";
 			List<TestStep> steps = new List<TestStep>();
+			string title;
+			IWebElement ele;
+			string date;
+			IJavaScriptExecutor js = (IJavaScriptExecutor)driver.GetDriver();
 			
 			if (step.Name.Equals("Verify Displayed Day on Top Scores")) {
 				TimeSpan time = DateTime.Now.TimeOfDay;
@@ -30,11 +34,44 @@ namespace SeleniumProject.Function
 				steps.Clear();
 			}
 			
-			else if (step.Name.Equals("Scroll Top Scores Page")) {
-				string title = "//div[contains(@class,'scores-date')]//div[contains(@class,'sm')]";
-				IWebElement ele = driver.FindElement("xpath", title);
-				string date = ele.GetAttribute("innerText");
-				IJavaScriptExecutor js = (IJavaScriptExecutor)driver.GetDriver();
+			else if (step.Name.Equals("Scroll Top Scores Page to Yesterday")) {
+				title = "//div[contains(@class,'scores-date')]//div[contains(@class,'sm')]";
+				ele = driver.FindElement("xpath", title);
+				date = ele.GetAttribute("innerText");
+				
+				if (date.Equals("TODAY")) {
+					js.ExecuteScript("window.scrollBy(0,-250)");
+					log.Info("Scrolling up on YESTERDAY...");
+					steps.Add(new TestStep(order, "Verify Displayed Day on Top Scores", "YESTERDAY", "verify_value", "xpath", title, wait));
+					TestRunner.RunTestSteps(driver, null, steps);
+					steps.Clear();
+				}
+				else {
+					log.Info("Page defaulted to YESTERDAY");
+				}
+			}
+			
+			else if (step.Name.Equals("Scroll Top Scores Page to Today")) {
+				title = "//div[contains(@class,'scores-date')]//div[contains(@class,'sm')]";
+				ele = driver.FindElement("xpath", title);
+				date = ele.GetAttribute("innerText");
+				
+				if (!date.Equals("TODAY")) {
+					do {
+						js.ExecuteScript("window.scrollBy(0,250)");
+						log.Info("Scrolling down on page...");
+						Thread.Sleep(1000);
+						ele = driver.FindElement("xpath", title);
+						date = ele.GetAttribute("innerText");
+					}
+					while (date.Equals("YESTERDAY"));
+				}
+			}
+			
+			else if (step.Name.Equals("Scroll Top Scores Page to Tomorrow")) {
+				title = "//div[contains(@class,'scores-date')]//div[contains(@class,'sm')]";
+				ele = driver.FindElement("xpath", title);
+				date = ele.GetAttribute("innerText");
 				
 				if (date.Equals("TODAY")) {
 					js.ExecuteScript("window.scrollBy(0,-250)");
