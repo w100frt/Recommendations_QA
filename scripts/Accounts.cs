fb87afc4a66288af80f6514e8d20bfd4ff20ea3e
codyphillips5 <codyphill5@gmail.com>
2020-04-23 18:12:01 -0400

    device id

---
+++ scripts/Accounts.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using SeleniumProject.Utilities;
+using OpenQA.Selenium;
+using log4net;
+using System.Threading;
+
+namespace SeleniumProject.Function
+{
+	public class Script : ScriptingInterface.IScript
+	{		
+		private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		public void Execute(DriverManager driver, TestStep step)
+		{
+			long order = step.Order;
+			string wait = step.Wait != null ? step.Wait : "";
+			List<TestStep> steps = new List<TestStep>();
+			IWebElement ele;
+			int size;
+			string data = "";
+			bool stop = false;
+			IJavaScriptExecutor js = (IJavaScriptExecutor)driver.GetDriver();
+			VerifyError err = new VerifyError();
+			
+			if (step.Name.Equals("Get/Compare Device ID")) {
+				data = js.ExecuteScript("wisRegistration.getDeviceID();");
+				log.Info(data);
+			}
+			
+			else {
+				throw new Exception("Test Step not found in script");
+			}
+		}
+	}
+}
