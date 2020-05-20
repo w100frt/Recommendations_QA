6afbaff2ced7ac301a3907ea84140c3082619f8c
codyphillips5 <codyphill5@gmail.com>
2020-05-20 13:29:19 -0400

    smokes dev

--- scripts/Explore.cs
+++ scripts/Explore.cs
@@ -19,30 +19,43 @@ namespace SeleniumProject.Function
 			string sport = step.Data;
 			string activeTeam;
             List<TestStep> steps = new List<TestStep>();
-            steps.Add(new TestStep(order, "Click Sport Menu Open", "", "click", "xpath", "//a[contains(@class,'entity-list')][div[div[contains(.,'"+ sport +"')]]]", wait));
-            TestRunner.RunTestSteps(driver, null, steps);
-			steps.Clear();
 			
-			var teams = driver.FindElements("xpath", "//div[contains(@class,'explore-basic-rows')]//a"); 
-			for (int i = 0; i < teams.Count; i++) {
-				int counter = i + 1;
-				activeTeam = driver.FindElement("xpath", "(//div[contains(@class,'explore-basic-rows')]//a)["+ counter +"]").GetAttribute("innerText");
-				
-				if (activeTeam.Equals("NFL")) {
-					activeTeam = "NATIONAL FOOTBALL LEAGUE";
-				}
-
-				steps.Add(new TestStep(order, "Click Team Name", "", "click", "xpath", "(//div[contains(@class,'explore-basic-rows')]//a)["+ counter +"]", wait));
-				steps.Add(new TestStep(order, "Verify Team Name in Header", activeTeam.ToUpper(), "verify_value", "xpath", "//div[contains(@class,'entity-header')]//div[contains(@class,'entity-title')]//span", wait));
+			if (step.Name.Equals("Verify Teams by Sport")) {
+			    steps.Add(new TestStep(order, "Click Sport Menu Open", "", "click", "xpath", "//a[contains(@class,'entity-list')][div[div[contains(.,'"+ sport +"')]]]", wait));
 				TestRunner.RunTestSteps(driver, null, steps);
 				steps.Clear();
 				
-				if (i < teams.Count - 1) {
-					steps.Add(new TestStep(order, "Click Explore", "", "click", "xpath", "//a[contains(@class,'explore-link')]", wait));
-					steps.Add(new TestStep(order, "Click Sport Menu Open", "", "click", "xpath", "//a[contains(@class,'entity-list')][div[div[contains(.,'"+ sport +"')]]]", wait));
+				var teams = driver.FindElements("xpath", "//div[contains(@class,'explore-basic-rows')]//a"); 
+				for (int i = 0; i < teams.Count; i++) {
+					int counter = i + 1;
+					activeTeam = driver.FindElement("xpath", "(//div[contains(@class,'explore-basic-rows')]//a)["+ counter +"]").GetAttribute("innerText");
+					
+					if (activeTeam.Equals("NFL")) {
+						activeTeam = "NATIONAL FOOTBALL LEAGUE";
+					}
+
+					steps.Add(new TestStep(order, "Click Team Name", "", "click", "xpath", "(//div[contains(@class,'explore-basic-rows')]//a)["+ counter +"]", wait));
+					steps.Add(new TestStep(order, "Verify Team Name in Header", activeTeam.ToUpper(), "verify_value", "xpath", "//div[contains(@class,'entity-header')]//div[contains(@class,'entity-title')]//span", wait));
 					TestRunner.RunTestSteps(driver, null, steps);
 					steps.Clear();
+					
+					if (i < teams.Count - 1) {
+						steps.Add(new TestStep(order, "Click Explore", "", "click", "xpath", "//a[contains(@class,'explore-link')]", wait));
+						steps.Add(new TestStep(order, "Click Sport Menu Open", "", "click", "xpath", "//a[contains(@class,'entity-list')][div[div[contains(.,'"+ sport +"')]]]", wait));
+						TestRunner.RunTestSteps(driver, null, steps);
+						steps.Clear();
+					}
 				}
+			}
+			
+			else if (step.Name.Equals("Click Entity by Data")) {
+				steps.Add(new TestStep(order, "Click " + step.Data, "", "click", "xpath", "//a[contains(@class,'entity-list-row-container')][div[contains(.,'"+ step.Data +"')]]", wait));
+				TestRunner.RunTestSteps(driver, null, steps);
+				steps.Clear();
+			}
+			
+			else {
+				throw new Exception("Test Step not found in script");
 			}
 		}
 	}
