f5540e346417b46a62032aa96f091eb1045afc5f
codyphillips5 <codyphill5@gmail.com>
2020-07-09 12:57:05 -0400

    video playback

--- scripts/LiveTV.cs
+++ scripts/LiveTV.cs
@@ -19,7 +19,7 @@ namespace SeleniumProject.Function
 			IWebElement ele;
 			int size = 0;
 			int channel = 0;
-			int attempts = 20;
+			int attempts = 50;
 			string classList = "";
 			List<TestStep> steps = new List<TestStep>();
 			VerifyError err = new VerifyError();
@@ -39,6 +39,25 @@ namespace SeleniumProject.Function
 				else {
 					log.Error("***Verification FAILED. Video returned " + classList + " ***");
 					err.CreateVerificationError(step, "playing", classList);
+					driver.TakeScreenshot(DataManager.CaptureMap["TEST_ID"] + "_verification_failure_" + DataManager.VerifyErrors.Count);
+				}
+			}
+			
+			else if (step.Name.Equals("Verify Video is Paused")) {
+				ele = driver.FindElement("xpath", "//div[@aria-label='Video Player']");
+				classList = ele.GetAttribute("className");
+				classList = classList.Substring(classList.IndexOf("jw-state-") + 9);
+				classList = classList.Substring(0, classList.IndexOf(" "));
+				do {
+					log.Info("Video State: " + classList);					
+				}
+				while (!classList.Equals("paused") && attempts-- > 0);
+				if (classList.Equals("paused")) {
+					log.Info("Verification PASSED. Video returned " + classList);
+				}
+				else {
+					log.Error("***Verification FAILED. Video returned " + classList + " ***");
+					err.CreateVerificationError(step, "paused", classList);
 					driver.TakeScreenshot(DataManager.CaptureMap["TEST_ID"] + "_verification_failure_" + DataManager.VerifyErrors.Count);
 				}
 			}
