a58b8cb2bb42d82db1ff8878f3cd63a58e818717
codyphillips5 <codyphill5@gmail.com>
2020-07-09 02:24:19 -0400

    video playback

--- scripts/LiveTV.cs
+++ scripts/LiveTV.cs
@@ -26,7 +26,8 @@ namespace SeleniumProject.Function
 			if (step.Name.Equals("Verify Video is Playing")) {
 				ele = driver.FindElement("xpath", "//div[@aria-label='Video Player']");
 				classList = ele.GetAttribute("className");
-				classList = classList.Substring(classList.IndexOf("jw-state-") + 27);
+				classList = classList.Substring(classList.IndexOf("jw-state-"));
+				classList = classList.Substring(0, classList.IndexOf(" "));
 				do {
 					log.Info(classList);					
 				}
