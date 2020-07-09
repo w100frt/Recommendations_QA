48c329d99e7a8966c19362a62f132832e19971e9
codyphillips5 <codyphill5@gmail.com>
2020-07-09 01:55:54 -0400

    video playback

--- scripts/LiveTV.cs
+++ scripts/LiveTV.cs
@@ -18,7 +18,7 @@ namespace SeleniumProject.Function
 			string wait = step.Wait != null ? step.Wait : "";
 			IWebElement ele;
 			int size = 0;
-			var classList = new List<string>();
+			string classList = "";
 			List<TestStep> steps = new List<TestStep>();
 			VerifyError err = new VerifyError();
 			
