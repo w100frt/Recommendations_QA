85940ef9fdf29032edbc391a1c6d762b401c0186
codyphillips5 <codyphill5@gmail.com>
2020-07-09 02:35:27 -0400

    video playback

--- scripts/LiveTV.cs
+++ scripts/LiveTV.cs
@@ -26,12 +26,20 @@ namespace SeleniumProject.Function
 			if (step.Name.Equals("Verify Video is Playing")) {
 				ele = driver.FindElement("xpath", "//div[@aria-label='Video Player']");
 				classList = ele.GetAttribute("className");
-				classList = classList.Substring(classList.IndexOf("jw-state-"));
+				classList = classList.Substring(classList.IndexOf("jw-state-") + 9);
 				classList = classList.Substring(0, classList.IndexOf(" "));
 				do {
-					log.Info(classList);					
+					log.Info("Video State: " + classList);					
 				}
 				while (!classList.Equals("playing") && attempts-- > 0);
+				if (classList.Equals("playing")) {
+					log.Info("Verification PASSED. Video returned " + classList);
+				}
+				else {
+					log.Error("***Verification FAILED. Video returned " + classList + " ***");
+					err.CreateVerificationError(step, "playing", classList);
+					driver.TakeScreenshot(DataManager.CaptureMap["TEST_ID"] + "_verification_failure_" + DataManager.VerifyErrors.Count);
+				}
 			}
 		}
 	}
