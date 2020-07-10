4ab51460bcad8f227b3ebce554baef395233ac52
codyphillips5 <codyphill5@gmail.com>
2020-07-10 17:08:36 -0400

    soccer scores

--- scripts/PlayByPlay.cs
+++ scripts/PlayByPlay.cs
@@ -21,11 +21,13 @@ namespace SeleniumProject.Function
 			int size;
 			int start;
 			int end;
+			int game = 0;
 			string data = "";
+			string status = "";
 			IJavaScriptExecutor js = (IJavaScriptExecutor)driver.GetDriver();
 			VerifyError err = new VerifyError();
 			
-			if (step.Name.Equals("Verify PBP Headers By Sport")) {
+			if (step.Name.Equals("Verify Final PBP Headers By Sport")) {
 				ele = driver.FindElement("xpath","//img[@class='location-image']");
 				data = ele.GetAttribute("src");
 				start = data.LastIndexOf('/')+1;
@@ -45,6 +47,79 @@ namespace SeleniumProject.Function
 					case "NCAABasketball" :
 						stoppage.Add("1ST HALF");
 						stoppage.Add("2ND HALF");
+						break;
+					case "MLB":
+						stoppage.Add("TOP 1ST");
+						stoppage.Add("BOTTOM 1ST");
+						stoppage.Add("TOP 2ND");
+						stoppage.Add("BOTTOM 2ND");
+						stoppage.Add("TOP 3RD");
+						stoppage.Add("BOTTOM 3RD");
+						stoppage.Add("TOP 4TH");
+						stoppage.Add("BOTTOM 4TH");
+						stoppage.Add("TOP 5TH");
+						stoppage.Add("BOTTOM 5TH");
+						stoppage.Add("TOP 6TH");
+						stoppage.Add("BOTTOM 6TH");
+						stoppage.Add("TOP 7TH");
+						stoppage.Add("BOTTOM 7TH");
+						stoppage.Add("TOP 8TH");
+						stoppage.Add("BOTTOM 8TH");
+						stoppage.Add("TOP 9TH");
+						stoppage.Add("BOTTOM 9TH");
+						break;
+					default :
+						stoppage.Add("1ST QUARTER");
+						stoppage.Add("2ND QUARTER");
+						stoppage.Add("3RD QUARTER");
+						stoppage.Add("4TH QUARTER");
+						break;
+				}
+				
+				foreach (string stop in stoppage) {
+					steps.Add(new TestStep(order, "Verify PBP Header for " + data, stop, "verify_value", "xpath", "(//div[contains(@class,'pbp-header')])["+size+"]", wait));
+					TestRunner.RunTestSteps(driver, null, steps);
+					steps.Clear();
+					size++;
+				}
+
+			}
+			
+			else if (step.Name.Equals("Verify Live PBP Header By Sport")) {
+				ele = driver.FindElement("xpath","//img[@class='location-image']");
+				data = ele.GetAttribute("src");
+				start = data.LastIndexOf('/')+1;
+				end = data.IndexOf(".vresize") - start;
+				data = data.Substring(start, end);
+				if (ele.GetAttribute("src").Contains("soccer")) {
+					data = "Soccer";
+				}
+				size = 1;
+
+				switch(data) {
+					case "NHL" :
+						stoppage.Add("1ST PERIOD");
+						stoppage.Add("2ND PERIOD");
+						stoppage.Add("3RD PERIOD");
+						break;
+					case "Soccer" :
+					case "NCAABasketball" :
+
+					status = driver.FindElement("//div[contains(@class,'status-line')]").Text.Substring(0,2);
+					try {
+						game = Int32.Parse(status);
+					}
+					catch (Exception e) {
+						log.Warn(status + " is not a number");
+					}
+					
+					if (game <= 45 || status.Equals("HT")) {
+						stoppage.Add("1ST HALF");
+					}
+					else {
+						stoppage.Add("1ST HALF");
+						stoppage.Add("2ND HALF");
+					}
 						break;
 					case "MLB":
 						stoppage.Add("TOP 1ST");
