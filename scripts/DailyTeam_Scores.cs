a1241b680d47ad3ee2d2eee73ad6ec1709685585
codyphillips5 <codyphill5@gmail.com>
2020-07-23 01:05:39 -0400

    scores updates

---
+++ scripts/DailyTeam_Scores.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using SeleniumProject.Utilities;
+using OpenQA.Selenium;
+using log4net;
+using System.Threading;
+
+namespace SeleniumProject.Function
+{
+	public class Script : ScriptingInterface.IScript
+	{		
+		private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		public void Execute(DriverManager driver, TestStep step)
+		{
+			long order = step.Order;
+			string wait = step.Wait != null ? step.Wait : "";
+			List<TestStep> steps = new List<TestStep>();
+			IWebElement ele;
+			string games = "";
+			string status = "";
+
+			if (step.Name.Equals("Verify Event")) {
+				if (DataManager.CaptureMap.ContainsKey("IN_SEASON")) {
+					DataManager.CaptureMap["GAME"] = step.Data;
+
+					ele = driver.FindElement("xpath", "(//a[contains(@class,'score-chip')])[" + step.Data +"]");
+					games = ele.GetAttribute("className");
+					games = games.Substring(games.IndexOf(" ") + 1); 
+					log.Info("Game State: " + games);
+					if (games.Equals("pregame")) {
+						step.Data = "TeamSport_FutureEvent";
+						DataManager.CaptureMap["EVENT_STATUS"] = "FUTURE";
+					}
+					else if (games.Equals("live")){
+						step.Data = "TeamSport_LiveEvent";
+						DataManager.CaptureMap["EVENT_STATUS"] = "LIVE";
+					}
+					else {
+						status = driver.FindElement("xpath", "(//a[contains(@class,'score-chip')])[" + step.Data +"]//div[contains(@class,'status-text')]").Text; 
+						log.Info("Event status: " + status);
+						if (status.Equals("POSTPONED") || status.Equals("CANCELED")) {
+							step.Data = "TeamSport_PostponedEvent";
+							DataManager.CaptureMap["EVENT_STATUS"] = "POSTPONED";
+						}
+						else {
+							step.Data = "TeamSport_PastEvent";
+							DataManager.CaptureMap["EVENT_STATUS"] = "FINAL";
+						}
+					}
+				}
+				else {
+					log.Warn("No IN_SEASON variable available or data is populated. Using data.");
+				}
+				
+				steps.Add(new TestStep(order, "Run Event Template", step.Data, "run_template", "xpath", "", wait));
+				TestRunner.RunTestSteps(driver, null, steps);
+				steps.Clear();
+			}
+			
+			else {
+				throw new Exception("Test Step not found in script");
+			}
+
+		}
+	}
+}
