14d152e9544f4067d746545cff624734f309c2bb
codyphillips5 <codyphill5@gmail.com>
2020-10-23 13:29:38 -0400

    nfl odds/cfb entity

--- scripts/Odds.cs
+++ scripts/Odds.cs
@@ -118,6 +118,19 @@ namespace SeleniumProject.Function
 						driver.TakeScreenshot(DataManager.CaptureMap["TEST_ID"] + "_verification_failure_" + DataManager.VerifyErrors.Count);
 					}
 				}
+			}	
+			
+			else if (step.Name.Equals("Verify Number of Event Props")) {
+				count = driver.FindElements("xpath","//div[contains(@class,'prop-bets-component')]/div[contains(@class,'prop-bets-event-title') or contains(.,'PROPS')]").Count;
+				
+				if (count >= 1 && count <= 5) {
+					log.Info ("Verication PASSED. " + count + " is between 1 and 5.");
+				}
+				else {
+					log.Error("***Verification FAILED. Props expected to be between 1 and 5. Actual total is " + count +  ". ***");
+					err.CreateVerificationError(step, "Between 1 and 5", count);
+					driver.TakeScreenshot(DataManager.CaptureMap["TEST_ID"] + "_verification_failure_" + DataManager.VerifyErrors.Count);
+				}
 			}
 			
 			else {
