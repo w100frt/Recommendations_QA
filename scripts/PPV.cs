5edff3c608ec9ad5562a6e624f6b98b077a8f24f
codyphillips5 <codyphill5@gmail.com>
2020-11-13 16:19:49 -0500

    ppv

--- scripts/PPV.cs
+++ scripts/PPV.cs
@@ -57,9 +57,6 @@ namespace SeleniumProject.Function
 			}
 			
 			else if (step.Name.Equals("Verify PPV Entitlement Count")) {
-				// temporary wait
-				Thread.Sleep(4000);	
-
 				test = (string) js.ExecuteScript("return document.readyState;");
 				while (!test.Equals("complete") && size++ < 5) {
 					log.Info("Waiting for readyState=complete");
