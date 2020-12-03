4b13d3ca8a9fa71e99f93de63eaa399f8e2c2241
truon <hao.truong@fox.com>
2020-12-03 11:03:48 -0500

    Added superSixList script

---
+++ scripts/SuperSixList.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using SeleniumProject.Utilities;
+using SeleniumProject;
+using OpenQA.Selenium;
+using log4net;
+using System.Collections.ObjectModel;
+
+namespace SeleniumProject.Function
+{
+    public class Script : ScriptingInterface.IScript
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void Execute(DriverManager driver, TestStep step)
+        {
+            long order = step.Order;
+            string wait = step.Wait != null ? step.Wait : "";
+            IWebElement ele;
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver.GetDriver();
+            var path = Path.Combine(Directory.GetCurrentDirectory());
+            log.Info($"Current Directory: {path}");
+        }
+    }
+}
