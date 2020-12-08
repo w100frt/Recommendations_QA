5513d45d3f6942a3666eb90850f3c72ac416adda
codyphillips5 <codyphill5@gmail.com>
2020-12-07 22:36:30 -0500

    clippers

--- scripts/Search.cs
+++ scripts/Search.cs
@@ -58,7 +58,7 @@ namespace SeleniumProject.Function
 					steps.Add(new TestStep(order, "Click Search", "", "click", "xpath", search, wait));
 					steps.Add(new TestStep(order, "Enter Search", ti.ToTitleCase(team.ToLower()), "input_text", "xpath", search, wait));
 					
-					if (team.Equals("MLB") || team.Equals("NBA") || team.Equals("NFL") || team.Equals("NHL") || team.Equals("SAN FRANCISCO 49ERS") || team.Equals("PHILADELPHIA 76ERS")) {
+					if (team.Equals("MLB") || team.Equals("NBA") || team.Equals("NFL") || team.Equals("NHL") || team.Equals("SAN FRANCISCO 49ERS") || team.Equals("PHILADELPHIA 76ERS") || team.Equals("LA CLIPPERS")) {
 						switch(team) {
 							case "MLB":
 								cat = "Major League Baseball";
@@ -77,6 +77,9 @@ namespace SeleniumProject.Function
 								break;
 							case "PHILADELPHIA 76ERS":
 								cat = "Philadelphia 76ers";
+								break;
+							case "LA CLIPPERS":
+								cat = "LA Clippers";
 								break;
 							default :
 								cat = "";
