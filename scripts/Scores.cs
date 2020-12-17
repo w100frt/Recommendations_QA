44bd984289c1413eb2842c69ca733439a7994d42
codyphillips5 <codyphill5@gmail.com>
2020-12-17 18:39:47 -0500

    scores vs stats

--- scripts/Scores.cs
+++ scripts/Scores.cs
@@ -103,7 +103,7 @@ namespace SeleniumProject.Function
 			}
 			
 			else if (step.Name.Equals("Scroll Top Scores Page to Tomorrow")) {
-				title = "//div[contains(@class,'scores-date')]//div[contains(@class,'sm')]";
+				title = "//div[contains(@class,'scores-date') or contains(@class,'week-selector')]//*[contains(@class,'sm-14')]";
 				ele = driver.FindElement("xpath", title);
 				date = ele.GetAttribute("innerText");
 				
