ead8b1653af57301704238e7fcddb8284ab79e3f
w100frt <forrest.thompson@fox.com>
2021-02-02 11:52:17 -0500

    t

--- scripts/Timestamp.cs
+++ scripts/Timestamp.cs
@@ -27,7 +27,7 @@ namespace SeleniumProject.Function
 			
 			if (step.Name.Equals("Mock Training Data Timestamp Input")) {
 				
-				MockTimestampA = driver.FindElements("xpath", "/html/body/div/main/form/div/div[1]/div[2]/input");
+				var MockTimestampA = driver.FindElements("xpath", "/html/body/div/main/form/div/div[1]/div[2]/input");
 			
 				if (MockTimestampA = "MM-DD-YYYY hh:mm:ss+ss:ss"){
 					log.Info("Verification Passed. Date Format Correct");
