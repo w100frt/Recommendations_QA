2aff9bcf713a51e4e0c44573f6e56171ebc00fe0
w100frt <forrest.thompson@fox.com>
2021-02-08 13:55:41 -0500

    t

--- scripts/Model_Instances.cs
+++ scripts/Model_Instances.cs
@@ -80,17 +80,11 @@ namespace SeleniumProject.Function
 					}
 				}
 				else {
-					xpath = "/html/body/div/main/div[10]/span"
+					xpath = "/html/body/div/main/div[10]/span";
 					ele = driver.FindElement("xpath", xpath);
 					data = ele.GetAttribute("textContent");
 					string  text = data.ToString();
 					log.Info(text);
-
-					ele = driver.FindElement("xpath", xpath);
-					data = ele.GetAttribute("textContent");
-					string  text = data.ToString();
-					int textLength = text.Length;
-					log.Info(textLength);
 
 
 					if(text != "No Model instances are available for this configuration") {
